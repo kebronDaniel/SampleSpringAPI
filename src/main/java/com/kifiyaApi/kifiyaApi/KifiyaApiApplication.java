d99b0062d46caea8404f66d1ab1ecad20dd6c6ab
Kebron <Kebronjrdaniel@gmail.com>
2022-10-12 12:35:10 +0300

    Inital commit and created a database connection

---
+++ src/main/java/com/kifiyaApi/kifiyaApi/KifiyaApiApplication.java
@@ -0,0 +1,13 @@
+package com.kifiyaApi.kifiyaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KifiyaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KifiyaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kifiyaApi/kifiyaApi/KifiyaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kifiyaApi.kifiyaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KifiyaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
